afb176809e634cfd71e84fc3cfd85382aee5cc94
gudebin <gudebin@outlook.com>
2023-01-09 22:07:43 +0800

    Site updated: 2023-01-09 22:07:43

---
+++ src/pages/User/Login/index.tsx
@@ -0,0 +1,87 @@
+import Footer from '@/components/Footer';
+import { login } from '@/services/ant-design-pro/api';
+import { getFakeCaptcha } from '@/services/ant-design-pro/login';
+import {
+  AlipayCircleOutlined,
+  LockOutlined,
+  MobileOutlined,
+  TaobaoCircleOutlined,
+  UserOutlined,
+  WeiboCircleOutlined,
+} from '@ant-design/icons';
+import {
+  LoginForm,
+  ProFormCaptcha,
+  ProFormCheckbox,
+  ProFormText,
+} from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import {
+  FormattedMessage,
+  history,
+  SelectLang,
+  useIntl,
+  useModel,
+  Helmet,
+} from '@umijs/max';
+import { Alert, message, Tabs } from 'antd';
+import Settings from '@/config/defaultSettings';
+import React, { useState } from 'react';
+import { flushSync } from 'react-dom';
+
+const ActionIcons = () => {
+  const langClassName = useEmotionCss(({ token }) => {
+    return {
+      marginLeft: '8px',
+      color: 'rgba(0, 0, 0, 0.2)',
+      fontSize: '24px',
+      verticalAlign: 'middle',
+      cursor: 'pointer',
+      transition: 'color 0.3s',
+      '&:hover': {
+        color: token.colorPrimaryActive,
+      },
+    };
+  });
+
+  return (
+    <>
+      <AlipayCircleOutlined
+        key="AlipayCircleOutlined"
+        className={langClassName}
+      />
+      <TaobaoCircleOutlined
+        key="TaobaoCircleOutlined"
+        className={langClassName}
+      />
+      <WeiboCircleOutlined
+        key="WeiboCircleOutlined"
+        className={langClassName}
+      />
+    </>
+  );
+};
+
+const Lang = () => {
+  const langClassName = useEmotionCss(({ token }) => {
+    return {
+      width: 42,
+      height: 42,
+      lineHeight: '42px',
+      position: 'fixed',
+      right: 16,
+      borderRadius: token.borderRadius,
+      ':hover': {
+        backgroundColor: token.colorBgTextHover,
+      },
+    };
+  });
+
+  return (
+    <div className={langClassName} data-lang>
+      {SelectLang && <SelectLang />}
+    </div>
+  );
+};
+
+const LoginMessage:
